a98f7ebd51f6e4938ffcb6fa84bf6daece8c4ff3
MiMo <michael.a.morris.public@gmail.com>
2020-12-13 17:40:16 -0600

    fix(feature-current-weather): ➕ Add morgan
    Add logging via morgan

--- index.mjs
+++ index.mjs
@@ -1,12 +1,14 @@
 import 'dotenv/config.js'
 import express from 'express'
 import jsend from 'jsend'
+import morgan from 'morgan'
 
 import currentWeatherRoutes from './routes/current.mjs'
 
 const app = express()
+app.use(morgan('dev'))
 
 app.use('/current', currentWeatherRoutes)
 app.use('/', (req, res) => res.json(jsend.success({ weather: 'sunny' })))
 
-app.listen(3000, () => console.log('Connected on port 3000'))
+app.listen(3001, () => console.log('Connected on port 3001'))
