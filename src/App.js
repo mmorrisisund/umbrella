4b09961bed060d33f74d0ea5dae443edd2b23716
MiMo <michael.a.morris.public@gmail.com>
2020-12-13 15:28:54 -0600

    feat(feature-main-layout): ✨ Add connection to server
    Connect search functionality to server.  Parse response into TempDisplay component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
+import { useEffect, useState } from 'react'
+
 import AtmosphereDisplay from './components/AtmosphereDisplay'
 import DetailDisplay from './components/DetailDisplay'
 import SearchBox from './components/SearchBox'
 import TempDisplay from './components/TempDisplay'
 
 function App () {
+  const [city, setCity] = useState('')
+  const [weather, setWeather] = useState(undefined)
+
+  useEffect(() => {
+    if (city) {
+      fetch(`/current/${city}`)
+        .then(resp => resp.json())
+        .then(({ data }) => setWeather(data))
+        .catch(console.log)
+    }
+  }, [city])
+
   return (
     <div className='bg-gradient-to-b from-blue-600 to-blue-300 min-h-screen min-w-max'>
       <header className='h-16 border-b border-blue-900'>
         <h1>umbrella</h1>
       </header>
       <main>
-        <SearchBox />
+        <SearchBox onSubmit={setCity} />
 
-        <TempDisplay />
+        {weather && (
+          <TempDisplay
+            cityName={weather.name}
+            time={weather.dt}
+            temp={weather.main.temp}
+            feelsLike={weather.main.feels_like}
+          />
+        )}
 
         <div className='pt-6 container mx-auto flex justify-center'>
           <div className='w-1/2 flex text-blue-100'>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import { useState } from 'react'
+
 import SearchInput from './SearchInput'
 
-const SearchBox = () => {
+const SearchBox = ({ onSubmit }) => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    onSubmit(searchTerm)
+  }
+
   return (
     <div className='container mx-auto flex justify-center'>
-      <form className='mx-auto mt-4 w-1/2'>
+      <form className='mx-auto mt-4 w-1/2' onSubmit={handleSubmit}>
         <label className='block'>
           <span className='text-blue-100 text-2xl tracking-wide'>
             Enter a city
           </span>
-          <SearchInput />
+          <SearchInput onTermChange={setSearchTerm} />
         </label>
       </form>
     </div>
