17868d1809b88c8bf5a9cca800139f2ed1b60f15
MiMo <michael.a.morris.public@gmail.com>
2020-12-20 21:27:39 -0600

    refactor(feature-current-weather): ♻️ Use simpler routing
    Update to use query params to simply requests

--- controllers/current.mjs
+++ controllers/current.mjs
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import jsend from 'jsend'
 
-const baseUrl = process.env.OPEN_WEATHER_URL
-const apiKey = process.env.OPEN_WEATHER_API_KEY
+const weatherUrl = process.env.OPEN_WEATHER_URL
+const weatherApiKey = process.env.OPEN_WEATHER_API_KEY
+const googleUrl = process.env.GOOGLE_API_URL
+const googleApiKey = process.env.GOOGLE_API_KEY
 
 export const getCurrentWeatherByCityName = async (req, res, next) => {
   const { name } = req.params
@@ -14,6 +16,50 @@ export const getCurrentWeatherByCityName = async (req, res, next) => {
 
     res.json(jsend.success(data))
   } catch (error) {
-    res.json(jsend.error({ message: error }))
+    console.log(error)
+    res.json(
+      jsend.error({
+        message: 'City could not be found.',
+        code: error.response.status
+      })
+    )
   }
+}
+
+export const getCurrentWeather = async (req, res) => {
+  const { lat, lng, city, state, name } = req.query
+
+  if (lat && lng) {
+    const data = await getDataByLatLng(lat, lng)
+
+    res.json(jsend.success(data))
+  }
+}
+
+async function getDataByLatLng (lat, lng) {
+  try {
+    const weatherPromise = axios.get(
+      `${weatherUrl}weather?lat=${lat}&lon=${lng}&units=imperial&appid=${weatherApiKey}`
+    )
+    const locationPromise = axios.get(
+      `${googleUrl}json?latlng=${lat},${lng}&key=${googleApiKey}`
+    )
+    const results = await Promise.all([weatherPromise, locationPromise])
+
+    return processResults(results[0], results[1])
+  } catch (error) {
+    return {
+      message: 'City could not be found.',
+      code: error.response.status
+    }
+  }
+}
+
+function processResults (weatherResult, locationResult) {
+  let result = {}
+
+  if (weatherResult) result.weather = weatherResult.data
+  if (locationResult) result.location = locationResult.data
+
+  return result
 }
--- routes/current.mjs
+++ routes/current.mjs
@@ -1,9 +1,11 @@
 import { Router } from 'express'
+import { getCurrentWeather } from '../controllers/current.mjs'
 
 import { getCurrentWeatherByCityName } from '../controllers/current.mjs'
 
 const router = Router()
 
-router.get('/:name', getCurrentWeatherByCityName)
+// router.get('/:name', getCurrentWeatherByCityName)
+router.get('/', getCurrentWeather)
 
 export default router
