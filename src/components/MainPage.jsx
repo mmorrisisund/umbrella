c6ee37330a957f8ccaf1325a41fa23b150af6524
MiMo <michael.a.morris.public@gmail.com>
2020-12-23 21:38:42 -0600

    feat: ✨ Add request to get weather and location data
    Add calls to server to obtain location and weather data

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from 'react'
 import Forecast from './Forecast'
 import LocationTitle from './LocationTitle'
 import TemperatureDisplay from './TemperatureDisplay'
 
 const MainPage = () => {
+  const [location, setLocation] = useState()
+  const [coords, setCoords] = useState()
+  const [weather, setWeather] = useState()
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => setCoords(coords),
+      // ({ code }) => {}
+      console.log
+    )
+  }, [])
+
+  useEffect(() => {
+    if (coords) {
+      fetch(`api/?lat=${coords.latitude}&lng=${coords.longitude}`)
+        .then(res => res.json())
+        .then(({ data }) => {
+          setWeather(data.weather)
+          setLocation(data.location)
+        })
+        .catch(console.log)
+    }
+  }, [coords])
+
   return (
     <div>
       <main className='mt-2'>
-        <LocationTitle />
+        <LocationTitle location={location} />
         <TemperatureDisplay />
         <Forecast />
       </main>
