de142c8dd25f03af67dabebf9aa539e6dcce3590
MiMo <michael.a.morris.public@gmail.com>
2020-12-23 22:34:19 -0600

    feat: ✨ Add rounding to temps
    Round all temps to nearest integer

--- src/components/TemperatureDisplay.jsx
+++ src/components/TemperatureDisplay.jsx
@@ -4,13 +4,15 @@ const TemperatureDisplay = ({ temperature, feelsLike, high, low }) => {
   return (
     <section className='flex flex-col items-center justify-center py-8'>
       <WiDaySunnyOvercast className='w-64 h-64 text-blue-100' />
-      <span className='text-blue-100 text-8xl'>{temperature}&#176;</span>
+      <span className='text-blue-100 text-8xl'>
+        {Math.round(temperature)}&#176;
+      </span>
       <span className='text-lg text-blue-100'>
-        Feels like {feelsLike}&#176;
+        Feels like {Math.round(feelsLike)}&#176;
       </span>
       <div className='flex justify-between mt-4 space-x-20 text-4xl text-blue-100 xl:text-5xl'>
-        <span className='text-red-700'>{high}&#176;</span>
-        <span className='text-indigo-700'>{low}&#176;</span>
+        <span className='text-red-700'>{Math.round(high)}&#176;</span>
+        <span className='text-indigo-700'>{Math.round(low)}&#176;</span>
       </div>
     </section>
   )
