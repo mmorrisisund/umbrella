75e9a3bf47794a596f791848c726da6c24c3aaa5
MiMo <michael.a.morris.public@gmail.com>
2020-12-24 18:07:28 -0600

    feat: ✨ Add forecast data
    Add and format forecast data for five days

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,13 +1,38 @@
 import ForecastCard from './ForecastCard'
 
-const Forecast = () => {
+const Forecast = ({ forecast }) => {
   return (
     <section className='flex flex-col pb-5 space-y-6 md:space-y-0 md:flex-row md:justify-evenly sm:px-16 xl:text-3xl'>
-      <ForecastCard />
-      <ForecastCard />
-      <ForecastCard />
-      <ForecastCard />
-      <ForecastCard />
+      <ForecastCard
+        date={forecast[1].dt}
+        high={forecast[1].temp.max}
+        low={forecast[1].temp.min}
+        description={forecast[1].weather[0].description}
+      />
+      <ForecastCard
+        date={forecast[2].dt}
+        high={forecast[2].temp.max}
+        low={forecast[2].temp.min}
+        description={forecast[2].weather[0].description}
+      />
+      <ForecastCard
+        date={forecast[3].dt}
+        high={forecast[3].temp.max}
+        low={forecast[3].temp.min}
+        description={forecast[3].weather[0].description}
+      />
+      <ForecastCard
+        date={forecast[4].dt}
+        high={forecast[4].temp.max}
+        low={forecast[4].temp.min}
+        description={forecast[4].weather[0].description}
+      />
+      <ForecastCard
+        date={forecast[5].dt}
+        high={forecast[5].temp.max}
+        low={forecast[5].temp.min}
+        description={forecast[5].weather[0].description}
+      />
     </section>
   )
 }
--- src/components/ForecastCard.jsx
+++ src/components/ForecastCard.jsx
@@ -1,12 +1,13 @@
+import dayjs from 'dayjs'
 import { WiDaySunnyOvercast } from 'react-icons/wi'
 
-const ForecastCard = ({ day, high, low, description }) => {
+const ForecastCard = ({ date, high, low, description }) => {
   return (
     <article className='flex items-center justify-between px-8 text-lg font-semibold text-blue-100 xl:text-2xl md:flex-col'>
-      <span>{day}</span>
+      <span>{dayjs.unix(date).format('dddd')}</span>
       <div className='flex items-center justify-between md:flex-col-reverse md:mt-2'>
         <span className='md:mt-2'>
-          {high}/{low}
+          {Math.round(high)} / {Math.round(low)}
         </span>
         <div className='ml-2 md:ml-0'>
           <WiDaySunnyOvercast className='w-10 h-10' />
