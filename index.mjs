a775c9b7add30e92498cafe0f829c9d92a517abd
MiMo <michael.a.morris.public@gmail.com>
2020-12-24 20:04:11 -0600

    feat: ✨ Send client app to all non-api routes
    Update routing to send client app

--- index.mjs
+++ index.mjs
@@ -1,14 +1,16 @@
 import 'dotenv/config.js'
+import path from 'path'
 import express from 'express'
-import jsend from 'jsend'
 import morgan from 'morgan'
 
 import apiRoutes from './routes/api.mjs'
 
 const app = express()
 app.use(morgan('dev'))
-
+app.use(express.static(path.join(process.cwd(), '..', 'client', 'build')))
 app.use('/api', apiRoutes)
-app.use('/', (req, res) => res.json(jsend.success({ weather: 'sunny' })))
+app.use('/', (req, res) =>
+  res.sendFile(path.join(process.cwd(), '..', 'client', 'build', 'index.html'))
+)
 
 app.listen(3001, () => console.log('Connected on port 3001'))
